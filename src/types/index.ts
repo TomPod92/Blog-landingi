7234d3161355faf286444f6bcb49dca00e8a57f3
tpodsiadlik <tpodsiadlik@FP-LSC0166>
2021-06-27 17:06:07 +0200

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export interface User {
+    id: string;
+    mail: string;
+    name: string;
+}
+
+export interface Post {
+    id: number;
+    userId: number;
+    title: string;
+}
+
+export interface Comment {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
